165ed99a7988207786539eb77bc2f9ea996fc98b
davecat <davemail@foxmail.com>
2017-11-29 18:00:20 +0800

    环境变量配置

---
+++ src/pages/report.js
@@ -0,0 +1,12 @@
+/**
+ * Created by dave on 2017/11/27.
+ */
+import React from 'react'
+class Report extends React.Component {
+    render() {
+        return (
+            <h1>报表</h1>
+        )
+    }
+}
+export default Report
